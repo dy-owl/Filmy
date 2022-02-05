9a909c24e2b9d9dab8d97cede7b0caee7ff69a6b
Dmytro Yantsybaiev <d.yantsybaiev@icloud.com>
2022-02-05 14:33:18 +0200

    Initial Commit

---
+++ Filmy/FilmyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmyApp.swift
+//  Filmy
+//
+//  Created by Dmytro Yantsybaiev on 05.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct FilmyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
